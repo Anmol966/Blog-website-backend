7899b4ff3d5c15cdf1a9cdefab6eead7333d1d8a
Anmol966 <anmolpanwar966@gmail.com>
2023-02-07 17:12:01 +0530

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const cors = require('cors')
 const express = require('express')
 
 const blogRoutes = require('./routes/blogs')
@@ -10,6 +11,7 @@ const app = express();
 
 // middleware
 app.use(express.json())
+app.use(cors)
 
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
